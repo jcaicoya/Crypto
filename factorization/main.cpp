83405d7fe7f6549b7ef4506c11e37f6229531e61
Caico <caicoya@gmail.com>
2025-05-17 21:45:05 +0200

    Factorize numbers

--- factorization/main.cpp
+++ factorization/main.cpp
@@ -4,7 +4,7 @@
 #include <iostream>
 #include <fstream>
 #include <exception>
-#include <__msvc_filebuf.hpp>
+#include <map>
 
 std::ostream & print(const BigUint& a, std::ostream &out = std::cout) {
     out << a.to_base10_string() << " <--> " << a.to_string();
@@ -34,22 +34,18 @@ std::size_t count_number_of_lines(const std::filesystem::path &path) {
     return line_counter;
 }
 
-using FactorTable = std::vector<std::vector<BigUint>>;
+using FactorTable = std::map<BigUint, std::vector<BigUint>>;
 using PrimeNumbers = std::vector<BigUint>;
 
 [[nodiscard]] FactorTable build_factor_table_from_file(const std::filesystem::path &path) {
-    FactorTable factor_table;
-    const auto number_of_lines = count_number_of_lines(path);
-    factor_table.reserve(number_of_lines * 2 + 1);
-
     std::ifstream fin(path);
     if (!fin) {
         throw std::runtime_error("Could not open file " + path.string());
     }
 
+    FactorTable factor_table;
     std::string line;
     std::size_t line_counter = 0;
-    std::size_t table_position = 0;
     while (std::getline(fin, line)) {
         line_counter++;
         std::istringstream iss(line);
@@ -59,66 +55,136 @@ using PrimeNumbers = std::vector<BigUint>;
             continue;
         }
 
-        std::vector<BigUint> table_line;
+        BigUint number;
+        std::vector<BigUint> factors;
+        iss >> number;
         while (!iss.eof()) {
             try {
-                BigUint number;
-                iss >> number;
-                table_line.push_back(number);
+                BigUint factor;
+                iss >> factor;
+                factors.emplace_back(factor);
             } catch (const std::exception &e) {
                 throw std::runtime_error("Could not parse line " + std::to_string(line_counter) + ": " + e.what());
             }
         }
 
-        factor_table.push_back(table_line);
-        table_position++;
+        factor_table.emplace(number, factors);
     }
 
     return factor_table;
 }
 
+void print_number_and_its_factors(const BigUint &number, const std::vector<BigUint> &factors) {
+    std::cout << number << ' ';
+    if (factors.empty()) {
+        std::cout << " is prime";
+    }
+    else {
+        std::cout << " = " << factors[0];
+        for (std::size_t ii = 1; ii < factors.size(); ii++) {
+            std::cout << " * " << factors[ii];
+        }
+    }
+}
+
 void summarize_factor_table(const FactorTable &factor_table) {
-    std::size_t table_line_counter = 0;
-    for (const auto &number_and_factors : factor_table) {
-        if (number_and_factors.empty()) {
-            std::string error_message = "Factor table line ";
-            error_message += std::to_string(table_line_counter);
-            error_message += " is empty";
-            throw std::runtime_error(error_message.c_str());
-        }
-        table_line_counter++;
-
-        if (number_and_factors.size() == 1) {
-            std::cout << number_and_factors.front() << " has not been factorized\n";
-        }
-
-        std::cout << number_and_factors.front();
-        if (number_and_factors.size() == 2) {
-            std::cout << " is prime\n";
-        }
-        else {
-            std::cout << " = " << number_and_factors[1];
-            for (std::size_t ii = 2; ii < number_and_factors.size(); ii++) {
-                std::cout << " * " << number_and_factors[ii];
-            }
-            std::cout << '\n';
-        }
+    for (const auto &[number, factors] : factor_table) {
+        print_number_and_its_factors(number, factors);
+        std::cout << '\n';
     }
 }
 
 void load_primes_from_table(const FactorTable &factor_table, PrimeNumbers &prime_numbers) {
-    for (auto &table_line: factor_table) {
-        if (table_line.size() == 2) {
-            prime_numbers.push_back(table_line.front());
+    for (const auto &[number, factors] : factor_table) {
+        if (factors.empty()) {
+            prime_numbers.push_back(number);
         }
     }
 }
 
+BigUint next_prime(const PrimeNumbers &prime_numbers, const BigUint &prime) {
+    auto itr = prime_numbers.begin();
+    while (*itr != prime && itr != prime_numbers.end()) {
+        ++itr;
+    }
+
+    if (itr == prime_numbers.end()) {
+        std::string message = "Could not find prime ";
+        message += prime.to_base10_string();
+        throw std::runtime_error(message.c_str());
+    }
+
+    ++itr;
+    if (itr == prime_numbers.end()) {
+        std::string message = "Could not get next prime to ";
+        message += prime.to_base10_string();
+        throw std::runtime_error(message.c_str());
+    }
+
+    return *itr;
+}
+
+std::vector<BigUint> factorize(const BigUint &number, const FactorTable &factor_table, const PrimeNumbers &prime_numbers) {
+    if (number == BigUint::ZERO or number == BigUint::ONE) {
+        throw std::runtime_error("number cannot be zero or one");
+    }
+
+    const auto itr = factor_table.find(number);
+    if (itr != factor_table.end()) {
+        return itr->second;
+    }
+
+    BigUint possible_divisor = BigUint::TWO;
+    BigUint possible_quotient = BigUint::ZERO;
+    bool found_divisor = false;
+    while (!found_divisor && possible_divisor.square() <= number) {
+        const auto [quotient, remainder] = number.divide_by(possible_divisor);
+        if (remainder == BigUint::ZERO) {
+            found_divisor = true;
+            possible_quotient = quotient;
+        }
+        else {
+            possible_divisor = next_prime(prime_numbers, possible_divisor);
+        }
+    }
+
+    if (!found_divisor) {
+        return {};
+    }
+
+    auto factors = factor_table.at(possible_quotient);
+    if (factors.empty()) {
+        factors.emplace_back(possible_quotient);
+    }
+    factors.emplace_back(possible_divisor);
+    std::ranges::sort(factors);
+    return factors;
+}
+
+void write_number_and_factors_at_the_end_of_file(const BigUint &number, const std::vector<BigUint> &factors, const std::filesystem::path &path) {
+    std::ofstream out(path, std::ios::app);
+    if (!out) {
+        throw std::runtime_error("Could not open file " + path.string());
+    }
+
+    out << '\n' << number;
+    if (!factors.empty()) {
+        for (const auto &factor : factors) {
+            out << ' ' << factor;
+        }
+    }
+
+    out.close();
+}
+
 int main() {
-    std::filesystem::path path("resources/factorization.txt");
+    const std::filesystem::path file_path = std::filesystem::current_path() / "resources"/ "factorization.txt";
+    std::cout << "Working with " << file_path.string() << "\n";
+    std::cout << std::endl;
+
     FactorTable factor_table;
     try {
-        factor_table = build_factor_table_from_file(path);
+        factor_table = build_factor_table_from_file(file_path);
     } catch (const std::exception &e) {
         std::cout << e.what() << '\n';
     }
@@ -136,6 +202,23 @@ int main() {
     for (auto &prime_number : prime_numbers) {
         std::cout << prime_number << '\n';
     }
+
+    std::cout << '\n';
+    std::cout << "Factoring...\n";
+    std::cout << "------------\n";
+    for (int steps = 1; steps <= 5; steps++) {
+        BigUint number = factor_table.rbegin()->first;
+        number.me_plus_one();
+        const auto factors = factorize(number, factor_table, prime_numbers);
+        factor_table.emplace_hint(factor_table.end(), number, factors);
+        if (factors.empty()) {
+            prime_numbers.push_back(number);
+        }
+        write_number_and_factors_at_the_end_of_file(number, factors, file_path);
+        print_number_and_its_factors(number, factors);
+        std::cout << '\n';
+    }
+
     return 0;
 }
 
